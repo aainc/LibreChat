2b1bb0402ab09b5959c4c3c6db87d70d60d11588
happy_ryo <iwama.ryo.0731@gmail.com>
2023-10-18 17:53:34 +0900

    Added prompt template functionality

--- client/src/components/ui/Landing.tsx
+++ client/src/components/ui/Landing.tsx
@@ -20,9 +20,8 @@ export default function Landing() {
 
   const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    const { innerText } = e.target as HTMLButtonElement;
-    const quote = innerText.split('"')[1].trim();
-    setText(quote);
+    const promptText = e.currentTarget.getAttribute('data-prompt') || '';
+    setText(promptText);
   };
 
   return (
@@ -45,38 +44,67 @@ export default function Landing() {
               <button
                 onClick={clickHandler}
                 className="w-full rounded-md bg-gray-50 p-3 hover:bg-gray-200 dark:bg-white/5 dark:hover:bg-gray-900"
+                data-prompt={localize('aa_prompt_body1')}
               >
-                &quot;{localize('com_ui_example_quantum_computing')}&quot; →
+                &quot;{localize('aa_prompt_title1')}&quot; →
               </button>
               <button
                 onClick={clickHandler}
                 className="w-full rounded-md bg-gray-50 p-3 hover:bg-gray-200 dark:bg-white/5 dark:hover:bg-gray-900"
+                data-prompt={localize('aa_prompt_body2')}
               >
-                &quot;{localize('com_ui_example_10_year_old_b_day')}&quot; →
+                &quot;{localize('aa_prompt_title2')}&quot; →
               </button>
               <button
                 onClick={clickHandler}
                 className="w-full rounded-md bg-gray-50 p-3 hover:bg-gray-200 dark:bg-white/5 dark:hover:bg-gray-900"
+                data-prompt={localize('aa_prompt_body3')}
               >
-                &quot;{localize('com_ui_example_http_in_js')}&quot; →
+                &quot;{localize('aa_prompt_title3')}&quot; →
+              </button>
+              <button
+                onClick={clickHandler}
+                className="w-full rounded-md bg-gray-50 p-3 hover:bg-gray-200 dark:bg-white/5 dark:hover:bg-gray-900"
+                data-prompt={localize('aa_prompt_body4')}
+              >
+                &quot;{localize('aa_prompt_title4')}&quot; →
               </button>
             </ul>
           </div>
           <div className="mb-8 flex flex-1 flex-col gap-3.5 md:mb-auto">
             <h2 className="m-auto flex items-center gap-3 text-lg font-normal md:flex-col md:gap-2">
-              <LightningIcon />
-              {localize('com_ui_capabilities')}
+              <SunIcon />
+              {localize('com_ui_examples')}
             </h2>
             <ul className="m-auto flex w-full flex-col gap-3.5 sm:max-w-md">
-              <li className="w-full rounded-md bg-gray-50 p-3 dark:bg-white/5">
-                {localize('com_ui_capability_remember')}
-              </li>
-              <li className="w-full rounded-md bg-gray-50 p-3 dark:bg-white/5">
-                {localize('com_ui_capability_correction')}
-              </li>
-              <li className="w-full rounded-md bg-gray-50 p-3 dark:bg-white/5">
-                {localize('com_ui_capability_decline_requests')}
-              </li>
+              <button
+                onClick={clickHandler}
+                className="w-full rounded-md bg-gray-50 p-3 hover:bg-gray-200 dark:bg-white/5 dark:hover:bg-gray-900"
+                data-prompt={localize('aa_prompt_body5')}
+              >
+                &quot;{localize('aa_prompt_title5')}&quot; →
+              </button>
+              <button
+                onClick={clickHandler}
+                className="w-full rounded-md bg-gray-50 p-3 hover:bg-gray-200 dark:bg-white/5 dark:hover:bg-gray-900"
+                data-prompt={localize('aa_prompt_body6')}
+              >
+                &quot;{localize('aa_prompt_title6')}&quot; →
+              </button>
+              <button
+                onClick={clickHandler}
+                className="w-full rounded-md bg-gray-50 p-3 hover:bg-gray-200 dark:bg-white/5 dark:hover:bg-gray-900"
+                data-prompt={localize('aa_prompt_body7')}
+              >
+                &quot;{localize('aa_prompt_title7')}&quot; →
+              </button>
+              <button
+                onClick={clickHandler}
+                className="w-full rounded-md bg-gray-50 p-3 hover:bg-gray-200 dark:bg-white/5 dark:hover:bg-gray-900"
+                data-prompt={localize('aa_prompt_body8')}
+              >
+                &quot;{localize('aa_prompt_title8')}&quot; →
+              </button>
             </ul>
           </div>
           <div className="mb-8 flex flex-1 flex-col gap-3.5 md:mb-auto">
